a0c26ff3b5e4d86eb9ffb9fe247d7b3d8047710c
Sadaf <sadaf.ntk@gmail.com>
2022-12-16 16:46:55 +0000

    Arrays.js

---
+++ Arrays.js
@@ -0,0 +1,31 @@
+let favHolidaysDestination = [
+    "Venice, Italy",
+    "Florida, USA",
+    "Paris, France",
+    "Barcelona, Spain"
+]
+console.log(favHolidaysDestination[1])
+for(let i=0; i < favHolidaysDestination.length; i++){
+console.log(favHolidaysDestination[i])
+}
+
+let favFilms = [
+    "Titanic, Pretty women",
+    "Gladiator, Pukar",
+    "Devdas, Fatherhood"
+]
+console.log(favFilms[1])
+console.log(favHolidaysDestination.length)
+console.log(favHolidaysDestination.pop())
+
+let favouriteSongs = [
+    "I want it that way",
+    "I'm still alive",
+    "Quit playing game with my heart",
+    "My heart goes on",
+]
+    
+
+console.log(favouriteSongs)
+
+    
