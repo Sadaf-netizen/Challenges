492e30846239c5c58f54deb5d69f1183528e7f78
Sadaf <sadaf.ntk@gmail.com>
2022-12-16 16:37:56 +0000

    adding variables.js

---
+++ variables.js
@@ -0,0 +1,28 @@
+let firstName="Sadaf"
+let age=40
+let universitystudent = true
+console.log("Hello World")
+console.log(firstName)
+console.log(age)
+console.log(universitystudent)
+firstName = "Aisha"
+console.log(firstName)
+let modelofCar ="Volvo"
+console.log("I drive a " + modelofCar)
+let occupation = ("Software Developer")
+console.log("Hi, my name is" + firstName +". I'm a " +occupation+ " and I drive a " + modelofCar)
+console.log("Hi my name is ${firstName}. I'm a ${occupation} and I drive a ${modelofCar}.")
+// let a = 3
+// a = a + 5
+// console.log(a) 
+let a = 3
+a += 5
+console.log(a)
+let myName = "Sadaf"
+let myAge = "30"
+let favouriteColour = "Purple"
+console.log(`Hi, my name is ${myName}. I'm ${myAge} and my favourite colour is ${favouriteColour}.`)
+let breakFast = "Fried Eggs"
+let  lunch = "Biryani"
+let dinner = "Grilled Chicken"
+console.log(`In the breakfast I had ${breakFast}. I ate ${lunch} in lunch and for the dinner I had ${dinner}.`)
