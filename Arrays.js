1de8c1982220e803ea3ba523055f7509e720a529
Sadaf <sadaf.ntk@gmail.com>
2022-12-28 19:02:35 +0000

    Activities in Arrays.js

--- Arrays.js
+++ Arrays.js
@@ -1,31 +1,46 @@
-let favHolidaysDestination = [
+ let favHolidaysDestination = [
     "Venice, Italy",
-    "Florida, USA",
-    "Paris, France",
-    "Barcelona, Spain"
-]
+   "Florida, USA",
+     "Paris, France",
+     "Barcelona, Spain"
+ ]
 console.log(favHolidaysDestination[1])
 for(let i=0; i < favHolidaysDestination.length; i++){
 console.log(favHolidaysDestination[i])
 }
 
-let favFilms = [
-    "Titanic, Pretty women",
+// Activity 1, 2 and 3
+
+ let favFilms = [
+   "Titanic, Pretty women",
     "Gladiator, Pukar",
     "Devdas, Fatherhood"
-]
-console.log(favFilms[1])
+ ]
+ console.log(favFilms)
+ console.log(favFilms[1])
 console.log(favHolidaysDestination.length)
-console.log(favHolidaysDestination.pop())
+ console.log(favFilms.pop())
 
-let favouriteSongs = [
-    "I want it that way",
-    "I'm still alive",
-    "Quit playing game with my heart",
+// Activity 4
+ let favDishes = [
+    "Pizza, Burgers",
+   "Biryani,Sandwich",
+     "Pasta, Tikka"
+ ]
+ console.log(favDishes.shift())
+
+// Activity 5
+ let favouriteSongs = [
+    "I want it that way",   
+      "I'm still alive",    
+       "Quit playing game with my heart",
     "My heart goes on",
-]
+    "Escape"
+ ]
     
-
-console.log(favouriteSongs)
+// shift() removes the first array element
+// the pop method removes the lasr element fron an array
+// console.log(favouriteSongs)
+ console.log(favouriteSongs.pop())
 
     
