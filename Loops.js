0874145a6bc4bda7949e708a43654c88ed114dcd
Sadaf <sadaf.ntk@gmail.com>
2022-12-29 09:41:11 +0000

    Activity in loops.js

--- Loops.js
+++ Loops.js
@@ -1,3 +1,5 @@
+
+// Activity 1
 let favHolidaysDestinations = [
     "Venice, Italy",
     "Paris, France",
@@ -9,7 +11,8 @@ let favHolidaysDestinations = [
 for (let i = 0; i < favHolidaysDestinations.length;  i+=2) {
      console.log(favHolidaysDestinations[i])
  }
-
+// initial expression; condition; increment,decrement expressions do stuff
+// try this
  for (let i = 0; i <= 10; i++){
  console.log(i += 2)   
  }
@@ -29,6 +32,7 @@ for (let n = 0; n < 20; n++){
  lives--
 }
   console.log("Game over!!!")
+//   -- decrement means we move from 5 4 3 2 1
 
  let currentDiceRoll = "3"
  while (currentDiceRoll != 1){
@@ -36,23 +40,28 @@ for (let n = 0; n < 20; n++){
      currentDiceRoll = Math.floor(Math.random()*6)+1
  }
  console.log(currentDiceRoll)
+//  != (not equal to)
+// currentDiceRoll is a global variable
+// math.random generate no between 0 to 1 it will be a decimal no
+// Math.floor gives the whole no
+// +1 has to add because the no is between 1 to 5  and for dice we have to make 6
 
 // //  Activity 4
-// // /ARRAY FOR THE RANDOM NUMBERS
-// let numbers = [];
+// /ARRAY FOR THE RANDOM NUMBERS
+let numbers = [];
 
-// while(numbers.length < 6){
-//     // GENERATES A RANDOM NUMBER
-//     randomNumber = Math.floor(Math.random() * 100)
+while(numbers.length < 6){
+    // GENERATES A RANDOM NUMBER
+    randomNumber = Math.floor(Math.random() * 100)
 
-//     // PRINTS THE RANDOM NUMBER
-//     console.log(randomNumber)
+    // PRINTS THE RANDOM NUMBER
+    console.log(randomNumber)
 
-//     // STORES NUMBERS IN THE ARRAY
-//     numbers.push(randomNumber)
-// }
+    // STORES NUMBERS IN THE ARRAY
+    numbers.push(randomNumber)
+}
 
-// // Activity 2 loops
+// Activity 2 loops
 
  let newEvenNum = []
 
@@ -65,34 +74,76 @@ console.log(`In reverse order, the numbers between 0 and 20 are; ${newEvenNum}`)
 
 // // Activity 3
 
-// let oddNum = []
+let oddNum = []
 
-// for (let y=1; y <= 30; y++){
-//     if (y % 2 == 1)[
-//         (oddNum.push(y))
-//     ]
-// }
-// console.log(`The odd numbers between 1 and 30 are; ${oddNum}`) 
+for (let y=1; y <= 30; y++){
+    if (y % 2 == 1)[
+        (oddNum.push(y))
+    ]
+}
+console.log(`The odd numbers between 1 and 30 are; ${oddNum}`) 
 
 // //  activity 3
-// // define age as a random number
+// define age as a random number
 
-// let age = Math.floor(Math.random() * 20);
+let age = Math.floor(Math.random() * 20);
 
-// // while age is less than 18
+// while age is less than 18
 
-// while(age < 18){
+while(age < 18){
     
-// // log "You're a child!"
-// // assign a new random number to age
+// log "You're a child!"
+// assign a new random number to age
 
-//     console.log("You're a child!")
-//     age = Math.floor(Math.random() * 100)
-// }
+    console.log("You're a child!")
+    age = Math.floor(Math.random() * 100)
+}
 
-// //if age is 18 or more then return "You're an adult!"
+//if age is 18 or more then return "You're an adult!"
 
-// console.log("You're an adult!")
+console.log("You're an adult!")
+
+// Activity 6 / Generate a random number btw 1 and 30 six times
+// Check whether divisible by 7 or not, inform user of outcome
+
+let numberRandom = [];
+i = 0
+
+for (i = 0; i < 6;) {
+numberRandom = Math.floor(Math.random()*30)+1
+i++
+    console.log(`The random number is ${numberRandom}`)
+    if (numberRandom % 7 == 0) {
+        console.log(`${numberRandom} is divisible by 7\n`)
+    }
+    if (numberRandom % 7 != 0) {
+        console.log(`${numberRandom} is not divisible by 7\n`)
+     }
+}
+
+// Activity 5
+let favFilms = [
+    "Titanic",
+    "Gladiator",
+    "Bulbul",
+    "Pukar"
+]
+for (let i = 0; i < favFilms.length; i++) {
+    console.log(favFilms[i])
+
+// check if third film is Ghostbusters    
+
+if (filmNumber == 2 && films[2] == "Bulbul"){
+    console.log("Yoo it's Bulbul!")
+}   else if (filmNumber == 2 && films[2] != "Bulbul"){
+    console.log("Oh noo! We want Bulbul!")
+
+// for the first, second and fourth films just log the name of the film
+
+}   else{
+    console.log(films[filmNumber])
+}
+}
 
 // //  Activity 1
 // let music = "Disco"
@@ -246,14 +297,14 @@ console.log(`In reverse order, the numbers between 0 and 20 are; ${newEvenNum}`)
 // let townOfWork = "Chorlton"
 // let townOfHome = "Warrington"
 
-//  Activity 3 (loops)
-let oddNumbers = []
-for (let a = 0; a <= 20; a++){
-    if (a % 2 == 1) {
-        (oddNumbers.push(a))
-}
-}
-console.log(`The odd numbers between 0 and 20 are ;${oddNumbers}`)
+// //  Activity 3 (loops)
+// let oddNumbers = []
+// for (let a = 0; a <= 20; a++){
+//     if (a % 2 == 1) {
+//         (oddNumbers.push(a))
+// }
+// }
+// console.log(`The odd numbers between 0 and 20 are ;${oddNumbers}`)
 
 
 
