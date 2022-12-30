0420d47c24e9290366cb3147d636bc777a56245b
Sadaf <sadaf.ntk@gmail.com>
2022-12-30 19:58:57 +0000

    Activity in Functions.js

--- Functions.js
+++ Functions.js
@@ -1,14 +1,19 @@
 const holidayDestination = () => {
  console.log("Yay, I'm going on holidays!")
 }
-holidayDestination()
+ holidayDestination() //here you are calling the functions
 // activity1
 const sayHello = (Park, Snacks) => {
 console.log(`Hi there, how are you? Go to  with me and have snacks` )
 }
 sayHello()
+// Activity 2
+const sayHellow2 = (myName, drink) => {
+    console.log(`Hi I'm ${myName}. Do you like to have some ${drink} with me`)
+}
+sayHello("Sara", "Coke")
 
-const holidayDestination1 = (country,month) => {
+const holidayDestination1 = (country,month) => { //country and month are parameters)
 console.log(`Yay im going on holidays ${country} in ${month}. I can't wait.`)
 }
 holidayDestination1()
@@ -16,7 +21,7 @@ holidayDestination1()
 const holidayDestination2 = (country, month) => {
 console.log(`Yay I'm going on holidays to ${country} in ${month}. I can't wait`)
 }
-holidayDestination2 ("Spain", "June")
+holidayDestination2 ("Spain", "June") //spain and june are arguments
 
 const inventory = (productcode, departmentName, quantity) => {
     console.log(`${quantity}of ${productcode} sold from ${departmentName}department, need to updatestock by ${quantity}`)
