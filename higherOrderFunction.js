e37d815cb5cc7bcbed7628559823a46bf13ebcc2
Sadaf <sadaf.ntk@gmail.com>
2023-01-01 09:15:41 +0000

    higherorderFunctions.js

--- higherOrderFunction.js
+++ higherOrderFunction.js
@@ -1,9 +1,22 @@
-const add = (num1) => {
-    return (num2) => {
-        return num1 + num2;
+const whichGreeting = (timeofDay) => {
+    console.log(`Good ${timeofDay}`);
 }
-}
- console.log(add(2)(1));
- class Pet {
-    constructor(name, age, type, colour, breed, time)
- }
+const greet = (time, fn) => {
+    if(time < 1200){
+         fn("Morning");
+    }else if (time >= 1200 && time < 1800){
+         fn("Afternoon");
+    }else{
+         fn("Evening");
+    }
+    }
+    greet(1500, whichGreeting);
+
+
+    const add = (num1) => {
+        return (num2) => {
+             return num1+num2;
+        }
+    }
+//  console.log(add(2));  //return the function in main function
+console.log(add(2)(1)); // 1 was passed to the function that sit inside the main function
